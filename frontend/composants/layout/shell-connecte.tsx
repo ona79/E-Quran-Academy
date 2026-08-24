9c8561498352621c4aae5dabb4dab41095ac733c
ona79 <kassadiallo603@gmail.com>
2026-08-24 01:03:54 +0000

    ....

--- frontend/composants/layout/shell-connecte.tsx
+++ frontend/composants/layout/shell-connecte.tsx
@@ -241,7 +241,6 @@ export function ShellConnecte({ children, sansPadding = false }: { children: Rea
         liens={liens}
         ouverte={drawerOuvert}
         surFermeture={() => setDrawerOuvert(false)}
-        surDeconnexion={() => setModalDeconnexion(true)}
       />
 
       {/* Contenu principal */}
